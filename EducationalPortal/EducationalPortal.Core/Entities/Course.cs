dd820aec02238fe0747c637b95486c9370801d4d
Shchoholiev <sh.codework@gmail.com>
2022-02-21 15:59:58 +0200

    Added repository interface

--- EducationalPortal/EducationalPortal.Core/Entities/Course.cs
+++ EducationalPortal/EducationalPortal.Core/Entities/Course.cs
@@ -6,6 +6,8 @@ namespace EducationalPortal.Core.Entities
     {
         public string Name { get; set; }
 
+        public string Thumbnail { get; set; }
+
         public string Description { get; set; }
 
         public int Price { get; set; }
---
+++ EducationalPortal/EducationalPortal.Core/Repository/IGenericRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+
+namespace EducationalPortal.Core.Repository
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task Add(TEntity item);
+
+        Task Update(TEntity item);
+
+        Task Delete(TEntity item);
+
+        Task Delete(int id);
+
+        Task Save();
+
+        Task Attach(params object[] obj);
+
+        Task<TEntity> GetOne(int? id);
+
+        Task<TEntity> GetOne(int? id, params Expression<Func<TEntity, object>>[] includeProperties);
+
+        Task<IEnumerable<TEntity>> GetAll();
+
+        Task<IEnumerable<TEntity>> GetAll(params Expression<Func<TEntity, object>>[] includeProperties);
+
+        Task<IEnumerable<TEntity>> GetAll(Expression<Func<TEntity, bool>> predicate,
+                                   params Expression<Func<TEntity, object>>[] includeProperties);
+
+        Task<IEnumerable<TEntity>> GetPage(int pageSize, int pageNumber);
+    }
+}
