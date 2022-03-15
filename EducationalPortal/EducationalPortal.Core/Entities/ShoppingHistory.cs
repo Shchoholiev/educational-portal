25bdf9546fcedc6930fc71cd30ee81ac74012bc5
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-03-15 12:04:37 +0100

    Added CartItem and ShoppingHistory entities with configurations.

---
+++ EducationalPortal/EducationalPortal.Core/Entities/ShoppingHistory.cs
@@ -0,0 +1,13 @@
+﻿namespace EducationalPortal.Core.Entities
+{
+    public class ShoppingHistory : EntityBase
+    {
+        public User User { get; set; }
+
+        public Course Course { get; set; }
+
+        public double Price { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
---
+++ EducationalPortal/EducationalPortal.Infrastructure/FluentAPI/CartItemEntityConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using EducationalPortal.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EducationalPortal.Infrastructure.FluentAPI
+{
+    public class CartItemEntityConfiguration : IEntityTypeConfiguration<CartItem>
+    {
+        public void Configure(EntityTypeBuilder<CartItem> builder)
+        {
+            builder.HasOne<Course>(ci => ci.Course);
+
+            builder.HasOne<User>(ci => ci.User);
+        }
+    }
+}
