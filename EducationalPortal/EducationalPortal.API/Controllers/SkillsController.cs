fcbfbbf8e3680714f9ee8a0dc0d99c98dc564398
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-04-07 22:03:25 +0200

    Configured Skills Controllers for Rest API.

--- EducationalPortal/EducationalPortal.API/Controllers/SkillsController.cs
+++ EducationalPortal/EducationalPortal.API/Controllers/SkillsController.cs
@@ -1,19 +1,19 @@
 ﻿using EducationalPortal.Application.Paging;
 using EducationalPortal.Application.Repository;
 using EducationalPortal.Core.Entities;
-using EducationalPortal.API.Mapping;
-using EducationalPortal.API.ViewModels.CreateViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using EducationalPortal.Application.DTO;
 
 namespace EducationalPortal.API.Controllers
 {
     [Authorize(Roles = "Creator")]
+    [ApiController]
+    [Route("api/skills")]
     public class SkillsController : Controller
     {
         private readonly IGenericRepository<Skill> _skillsRepository;
-
-        private readonly Mapper _mapper = new();
 
         public SkillsController(IGenericRepository<Skill> skillsRepository)
         {
@@ -21,62 +21,60 @@ namespace EducationalPortal.API.Controllers
         }
 
         [HttpGet]
-        public async Task<PartialViewResult> Index(PageParameters pageParameters, List<Skill> chosenSkills)
+        public async Task<ActionResult<IEnumerable<Skill>>> GetSkills([FromQuery]PageParameters pageParameters)
         {
             var skills = await this._skillsRepository.GetPageAsync(pageParameters);
-            var skillCreateModels = this._mapper.Map(skills, chosenSkills);
-            var totalCount = await this._skillsRepository.GetCountAsync(s => true);
-            var pagedSkills = new PagedList<SkillCreateModel>(skillCreateModels, pageParameters, totalCount);
+            var metadata = new
+            {
+                skills.TotalItems,
+                skills.PageSize,
+                skills.PageNumber,
+                skills.TotalPages,
+                skills.HasNextPage,
+                skills.HasPreviousPage
+            };
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(metadata));
 
-            return PartialView("_AddSkillsPanel", pagedSkills);
+            return skills;
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(Skill skill)
+        public async Task<IActionResult> Create([FromBody]SkillDTO skillDTO)
         {
             if (ModelState.IsValid)
             {
-                if (await this._skillsRepository.Exists(s => s.Name == skill.Name))
+                if (await this._skillsRepository.Exists(s => s.Name == skillDTO.Name))
                 {
                     ModelState.AddModelError(string.Empty, "Skill already exists!");
                 }
                 else
                 {
+                    var skill = new Skill { Id = skillDTO.Id, Name = skillDTO.Name };
                     await this._skillsRepository.AddAsync(skill);
-                    return Json(new { success = true });
+                    return StatusCode(201);
                 }
             }
 
-            return PartialView("_CreateSkill", skill);
+            return BadRequest(ModelState);
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Add(int idSkills, List<Skill> skills)
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
         {
-            var skill = await this._skillsRepository.GetOneAsync(idSkills);
-            skills.Add(skill);
-            return PartialView("_Skills", skills);
-        }
-
-        [HttpPost]
-        public IActionResult Remove(int idSkills, List<Skill> skills)
-        {
-            skills.Remove(skills.FirstOrDefault(s => s.Id == idSkills));
-            return PartialView("_Skills", skills);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Delete(int idSkills)
-        {
-            if (await this._skillsRepository.Exists(s => s.CoursesSkills.Any(cs => cs.SkillId == idSkills)))
+            if (await this._skillsRepository.Exists(s => s.CoursesSkills.Any(cs => cs.SkillId == id)))
             {
-                return Json(new { success = false, message = "This skill is used in other courses!" });
+                return BadRequest("This skill is used in other courses!");
             }
             else
             {
-                var skill = await this._skillsRepository.GetOneAsync(idSkills);
+                var skill = await this._skillsRepository.GetOneAsync(id);
+                if (skill == null)
+                {
+                    return NotFound();
+                }
+
                 await this._skillsRepository.DeleteAsync(skill);
-                return Json(new { success = true });
+                return NoContent();
             }
         }
     }
