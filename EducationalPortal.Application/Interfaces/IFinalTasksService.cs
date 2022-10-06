82fd3dda30a3b046aab5608df94f19e11e65f22f
DESKTOP-J5SGACQ\rareb <sh.codework@gmail.com>
2022-10-05 21:32:16 -0400

    Added Interfaces for both service and repository.

---
+++ EducationalPortal.Application/Interfaces/IFinalTasksService.cs
@@ -0,0 +1,18 @@
+﻿using EducationalPortal.Application.Models.DTO.FinalTasks;
+using EducationalPortal.Application.Paging;
+
+namespace EducationalPortal.Application.Interfaces
+{
+    public interface IFinalTasksService
+    {
+        Task CreateAsync(FinalTaskDto finalTaskDto, CancellationToken cancellationToken);
+
+        Task<PagedList<FinalTaskDto>> GetPageAsync(PageParameters pageParameters, CancellationToken cancellationToken);
+
+        Task DeleteAsync(int id, CancellationToken cancellationToken);
+
+        Task SubmitAsync(FinalTaskDto finalTaskDto, CancellationToken cancellationToken);
+
+        Task ReviewAsync(FinalTaskDto finalTaskDto, CancellationToken cancellation);
+    }
+}
---
+++ EducationalPortal.Application/Interfaces/Repositories/IFinalTasksRepository.cs
@@ -0,0 +1,24 @@
+﻿using EducationalPortal.Application.Paging;
+using EducationalPortal.Core.Entities.FinalTasks;
+
+namespace EducationalPortal.Application.Interfaces.Repositories
+{
+    public interface IFinalTasksRepository
+    {
+        Task AddAsync(FinalTask finalTask, CancellationToken cancellationToken);
+
+        Task DeleteAsync(FinalTask finalTask, CancellationToken cancellationToken);
+
+        Task<FinalTask?> GetFinalTaskAsync(int id, CancellationToken cancellationToken);
+
+        Task<PagedList<FinalTask>> GetPageAsync(PageParameters pageParameters, CancellationToken cancellationToken);
+
+        Task<bool> ExistsAsync(string name, CancellationToken cancellationToken);
+
+        Task AddSudmittedFinalTaskAsync(SubmittedFinalTask finalTask, CancellationToken cancellationToken);
+
+        Task AddSudmittedReviewQuestionsAsync(IEnumerable<SubmittedReviewQuestion> questions, CancellationToken cancellationToken);
+
+        Task UpdateSudmittedFinalTaskAsync(SubmittedFinalTask finalTask, CancellationToken cancellationToken);
+    }
+}
