1da4b441f66ac2fff7094895d877ce7b83234923
DESKTOP-J5SGACQ\rareb <sh.codework@gmail.com>
2022-10-14 18:36:21 -0400

    Added certificate entity.

---
+++ EducationalPortal.Core/Entities/Courses/Certificate.cs
@@ -0,0 +1,19 @@
+﻿using EducationalPortal.Core.Common;
+
+namespace EducationalPortal.Core.Entities.Courses
+{
+    public class Certificate : EntityBase
+    {
+        public string VerificationCode { get; set; }
+
+        public DateTime DateOfCompletionUTC { get; set; }
+
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+
+        public int CourseId { get; set; }
+
+        public Course Course { get; set; }
+    }
+}
