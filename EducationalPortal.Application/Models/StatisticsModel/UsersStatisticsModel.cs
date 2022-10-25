eb75a68a1089f599c254e3effe87bb22e6c30e49
DESKTOP-J5SGACQ\rareb <sh.codework@gmail.com>
2022-10-25 11:23:26 -0400

    Added Statistics Models.

---
+++ EducationalPortal.Application/Models/StatisticsModel/UsersStatisticsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EducationalPortal.Application.Models.StatisticsModel
+{
+    public class UsersStatisticsModel
+    {
+        public List<UserStatisticsModel> Users { get; set; }
+
+        public int AverageCompletedCoursesPercentage { get; set; }
+    }
+}
